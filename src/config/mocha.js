8ba00b7bc4c8bd536d0ab498b04a78b24f429d1b
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2024-10-17 17:16:57 +0200

    [fix #14] Ignore some stuff in test files

--- src/config/mocha.js
+++ src/config/mocha.js
@@ -1,10 +1,34 @@
 import mochaPlugin from "eslint-plugin-mocha";
 
+import * as sections from "../sections.js";
+
 export const apply = (configResult, eslintConfig) => {
   if (!eslintConfig.mocha) return;
   const mochaFilter = ["src/tests/**/*", "**/*.test.*"];
-  configResult.push({
+  const mochaSection = {
     ...mochaPlugin.configs.flat.recommended,
     files: mochaFilter,
-  });
+  };
+  sections.configureRules(
+    mochaSection,
+    "",
+    {
+      "max-classes-per-file": "off",
+      "max-lines": "off",
+      "max-lines-per-function": "off",
+      "max-params": "off",
+      "no-magic-numbers": "off",
+    },
+  );
+  if (eslintConfig.typescript) {
+    sections.configureRules(
+      mochaSection,
+      "@typescript-eslint",
+      {
+        "max-params": "off",
+        "no-magic-numbers": "off",
+      },
+    );
+  }
+  configResult.push(mochaSection);
 };
