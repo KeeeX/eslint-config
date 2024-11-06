475959dd189e3a60ed935969b3b7e0dec997f872
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2024-11-06 17:47:32 +0100

    [fix #28] Removed `prefer-arrow-callback` from mocha test files

--- src/config/mocha.js
+++ src/config/mocha.js
@@ -1,26 +1,46 @@
 import mochaPlugin from "eslint-plugin-mocha";
 
+import {getFiles} from "../pathutils.js";
 import * as sections from "../sections.js";
 
 export const apply = (configResult, eslintConfig) => {
   if (!eslintConfig.mocha) return;
-  const mochaFilter = ["src/tests/**/*", "**/*.test.*"];
+  const fileTypes = {
+    javascript: true,
+    jsx: Boolean(eslintConfig.react),
+    typescript: eslintConfig.typescript,
+  };
+  const mochaFilter = getFiles([
+    {
+      filePatterns: "*.ext",
+      fileTypes,
+      recursiveDirectories: "src/tests",
+    },
+    {
+      filePatterns: "*.test.ext",
+      fileTypes,
+      recursiveDirectories: "src",
+    },
+  ]);
   const mochaSection = {
     ...mochaPlugin.configs.flat.recommended,
     files: mochaFilter,
   };
-  sections.configureRules(mochaSection, "", {
+  configResult.push(mochaSection);
+  const override = sections.getNamedSection(configResult, "keeex/mocha");
+  override.files = mochaFilter;
+  sections.configureRules(override, "", {
     "max-classes-per-file": "off",
     "max-lines": "off",
     "max-lines-per-function": "off",
     "max-params": "off",
     "no-magic-numbers": "off",
+    "prefer-arrow-callback": "off",
   });
   if (eslintConfig.typescript) {
-    sections.configureRules(mochaSection, "@typescript-eslint", {
+    sections.configureRules(override, "@typescript-eslint", {
       "max-params": "off",
       "no-magic-numbers": "off",
     });
   }
-  configResult.push(mochaSection);
 };
