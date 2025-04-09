eae7d901ac30c971999ce83d2f563aa134ef416e
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2025-04-09 15:14:39 +0200

    Removed old config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import eslintConfig from "./src/index.js";
 export default await eslintConfig({
   environments: "node",
   globals: [{globals: {node: true}}],
-  ignores: ["src_old"],
   mocha: false,
   typescript: false,
 });
